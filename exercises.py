40ed415c7f479c7fd718afd7afd177192b5e92b7
shahado09 <almurran.shahd@gmail.com>
2026-01-12 14:42:18 +0300

    update fizz

--- exercises.py
+++ exercises.py
@@ -61,12 +61,13 @@ weather_advice()
 def fizz_buzz():
     for i in range(1,51):
 
-        if i %3==0:
+        if i %3==0 and i%5==0:
+            print("FizzBuzz")
+        elif i %3==0:
             print("fizz")
         elif i %5==0:
             print("Buzz")
-        elif i %3==0 and i%5==0:
-            print("FizzBuzz")
+
 
 
 
